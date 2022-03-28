2a8ab660d5cf827c6bff08d405dd6e58713fa3fa
kazufumi.yoshimoto <kazufumi.yoshimoto2@gmail.com>
2022-03-28 11:21:28 +0000

    add column

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -18,6 +18,7 @@
                 <tr>
                     {{-- メッセージ詳細ページへのリンク --}}
                     <td>{!! link_to_route('messages.show', $message->id, ['message' => $message->id]) !!}</td>
+                    <td>{{ $message->title }}</td>
                     <td>{{ $message->content }}</td>
                 </tr>
                 @endforeach
